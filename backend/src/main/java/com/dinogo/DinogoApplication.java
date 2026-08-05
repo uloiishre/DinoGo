65884499fd22def70fa3a34e061ad88c8bd41f11
unknown <uloiishre898>
2026-08-06 03:54:13 +0800

    Initial backend project

---
+++ backend/src/main/java/com/dinogo/DinogoApplication.java
@@ -0,0 +1,13 @@
+package com.dinogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinogoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dinogo/DinogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
