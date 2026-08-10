3f6ea292be851e0c48dab6975a5536fa73908716
Ghowyang <tloml2130288@gmail.com>
2026-08-10 16:33:19 +0800

    feat:orderdetail orderlist

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomeView from '@/views/HomeView.vue'
+import OrderDetail from '@/views/OrderDetail.vue'
+import OrderList from '@/views/OrderList.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/orders',
+      name: 'order-list',
+      component: OrderList,
+    },
+    {
+      path: '/orders/:orderId',
+      name: 'order-detail',
+      component: OrderDetail,
+      props: true,
+    },
+  ],
 })
 
 export default router
