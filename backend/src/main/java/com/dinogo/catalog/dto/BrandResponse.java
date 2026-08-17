7194e3c5e7bf16c40e5570efee9d7833a77c6730
uloiishre898 <uloiishre898@gmail.com>
2026-08-17 14:22:50 +0800

    新增brandAPI初始建置

---
+++ backend/src/main/java/com/dinogo/catalog/dto/BrandResponse.java
@@ -0,0 +1,12 @@
+package com.dinogo.catalog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BrandResponse {
+
+    private Integer brandId;
+    private String brandName;
+}
