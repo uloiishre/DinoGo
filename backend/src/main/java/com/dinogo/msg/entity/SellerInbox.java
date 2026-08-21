967a804831db72bd74bde3da62c7ec41fcb2a618
chensr6907 <chensr6907@gmail.com>
2026-08-21 15:56:42 +0800

    sysmsg

--- backend/src/main/java/com/dinogo/msg/entity/SellerInbox.java
+++
@@ -1,9 +0,0 @@
-// package com.dinogo.msg.entity;
-
-// public enum SellerInbox {
-
-// SYSTEM_NOTICE, // OA、OS
-// NEW_ORDER, // AS + PAID
-// PROGRESS_ORDER, // AS + SHIPPED / CANCELLED
-// COMPLETED_ORDER // AS + COMPLETED
-// }
